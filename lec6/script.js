09b645b7d1076c51bab04d7c2d07cb425961e31a
pankaj desai <er.pankajdesai90>
2023-05-23 16:55:57 +0530

    Lect6 of String and Object

---
+++ lec6/script.js
@@ -0,0 +1,86 @@
+
+// string and object
+
+let name = "This is Rhaul Raj";
+
+// let x = name.substring(1, 5);
+// x = name.substring(3, -4);
+// x = name.substring(5);
+
+// let x=name.substr(5);
+// x=name.substr(1,-4);
+// x=name.substr(-2);// print from negative side
+// x=name.substr(1,6);// 6 repersent total numbe of character
+
+
+
+//let newname=name.slice(1);
+// console.log(name.slice(2));
+// console.log(name.slice(1,4));
+// console.log(name.slice(1,-2));
+
+// convert string into araay
+
+let x = name.split(' ');
+x=name.split("is");
+
+
+
+console.log(x);
+console.log(name);
+
+//console.log(newname);
+
+// Object
+
+// const person = {
+//     name: "rahul",
+//     age: 33,
+//     hobbies: ["cricket", "chess", "Movie"]
+// }
+
+// // we can add property using dot notation and bracket notation
+
+// person["eno"] = 22323;
+// person.haskid = true;
+
+// // if we want to add property like full name we use bracket notation we can't do with dot
+// person["full name"]="rahul raj";
+
+// for(let key in person)
+// {
+//     console.log(`${key} is ${person[key]}`)
+// }
+
+//  console.log(typeof (Object.keys(person)));
+//  const val = Array.isArray((Object.keys(person)));
+//  console.log(val);
+// console.log(person["hobbies"])
+// console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
